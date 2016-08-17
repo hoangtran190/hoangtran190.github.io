bfa2b13b97813f39ba6d7fb746923a0759068696
hoangtran190 <jesse.alone@yahoo.com>
2016-08-17 14:21:35 +0700

    calculator

---
+++ quiz/js.js
@@ -0,0 +1,17 @@
+var correctAnswer = 0;
+function correctAnswer(){
+	alert("Correct!")
+	var answer=0;
+	return answer +=1;
+	result();
+}
+function reset(){
+	return answer = 0;
+}
+function wrongAnswer(){
+
+}
+
+function result(){
+	
+}
